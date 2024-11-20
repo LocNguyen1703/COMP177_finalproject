5ea9455665571769827ac063651c8d8c761ee694
Loc Nguyen <h_nguyen128@u.pacific.edu>
2024-11-20 15:48:26 -0800

    fixed graph-drawing code - code can now highlight shortest path every time

--- dynamic_routing.py
+++ dynamic_routing.py
@@ -9,7 +9,7 @@ def Dijkstra(matrix, root, dest):
 edge_list = [(1, 2), (2, 3), (2, 5), (3, 4), (4, 5), (5, 6), (6, 7)]
 graph = nx.from_edgelist(edgelist=edge_list)
 
-shortest_path = Dijkstra(graph, 4, 7)
+shortest_path = Dijkstra(graph, 3, 7)
 
 # co-pilot generated shiet
 # nx.draw_spring(graph, with_labels=True, edge_color='r', nodelist=shortest_path, edgelist=[(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)])
@@ -17,8 +17,12 @@ shortest_path = Dijkstra(graph, 4, 7)
 # drawing this way below will show the nodes in the shortest path in blue
 # nx.draw_spring(graph, with_labels=True, nodelist=shortest_path, edgelist=edge_list, edge_color='r', node_color='b')
 
-edge_colors = ['r' if (u, v) in zip(shortest_path[:-1], shortest_path[1:]) else 'b' for u, v in edge_list]
+#zip function creates a zip object that's basically a list of tuples that represents edges in the shortest path
+edge_colors = ['r' if (u, v) in zip(shortest_path[:-1], shortest_path[1:]) or (v, u) in zip(shortest_path[:-1], shortest_path[1:]) else 'b' for u, v in edge_list]
 nx.draw_spring(graph, with_labels=True, edge_color=edge_colors)
+
+print(shortest_path[:-1]) # for debugging
+print(shortest_path[1:]) # for debugging
 
 plt.axis=("equal")
 plt.show()
