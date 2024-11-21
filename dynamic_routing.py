3e7c1556f748886ec274f3b7e838b7e5b3996516
Loc Nguyen <h_nguyen128@u.pacific.edu>
2024-11-20 18:50:16 -0800

    fixed the code for creating edge_colors list by using graph.edges

--- dynamic_routing.py
+++ dynamic_routing.py
@@ -6,21 +6,21 @@ def Dijkstra(matrix, root, dest):
     # return NotImplementedError
     return nx.dijkstra_path(matrix, root, dest)
 
-nparr = np.array([[0, 1, 0, 0, 0, 0, 0, 0],
-                  [1, 0, 1, 0, 1, 0, 0, 0],
-                  [0, 1, 0, 1, 0, 0, 0, 0],
-                  [1, 0, 0, 0, 0, 0, 0, 1],
-                  [0, 1, 0, 0, 0, 0, 0, 0],
-                  [1, 0, 0, 0, 0, 0, 0, 1],
-                  [1, 1, 0, 0, 0, 0, 0, 1],
-                  [0, 0, 0, 1, 0, 1, 1, 0]
+nparr = np.array([[0, 1, 1, 0, 0, 0, 0, 0, 0],
+                  [1, 0, 0, 1, 0, 1, 0, 0, 0],
+                  [1, 0, 0, 1, 0, 0, 0, 0, 0],
+                  [0, 1, 1, 0, 1, 0, 1, 0, 0],
+                  [0, 0, 0, 1, 0, 1, 0, 1, 1],
+                  [0, 1, 0, 0, 1, 0, 0, 0, 1],
+                  [0, 0, 0, 1, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 1, 0, 1, 0, 1],
+                  [0, 0, 0, 0, 1, 1, 0, 1, 0]
                 ])
-edge_list = [(i, j) for i in range(len(nparr)) for j in range(len(nparr[i])) if nparr[i][j] == 1]
 
 graph = nx.from_numpy_array(nparr)
 
-shortestPathNodes = Dijkstra(graph, 4, 7)
-
+shortestPathNodes = Dijkstra(graph, 2, 8)
+print(shortestPathNodes) # for debugging
 # co-pilot generated shiet
 # nx.draw_spring(graph, with_labels=True, edge_color='r', nodelist=shortest_path, edgelist=[(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)])
 
@@ -30,12 +30,10 @@ shortestPathNodes = Dijkstra(graph, 4, 7)
 # zip function creates a zip object that's basically a list of tuples that represents edges in the shortest path
 # tuple() reassigns the zip object to a tuple object
 shortestPathEdges = tuple(zip(shortestPathNodes[:-1], shortestPathNodes[1:]))
-
-edge_colors = ['r' if (u, v) in shortestPathEdges or (v, u) in shortestPathEdges else 'b' for u, v in edge_list]
-nx.draw_spring(graph, with_labels=True, edge_color=edge_colors)
-
-print(shortestPathNodes[:-1]) # for debugging
-print(shortestPathNodes[1:]) # for debugging
+print(shortestPathEdges) # for debugging
+edge_colors = ['r' if edge in shortestPathEdges or (edge[1], edge[0]) in shortestPathEdges else 'b' for edge in graph.edges]
+print(edge_colors) # for debugging
+nx.draw(graph, pos=nx.spring_layout(graph), with_labels=True, edge_color=edge_colors)
 
 plt.axis=("equal")
 plt.show()
